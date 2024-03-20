355556071f734ca5b5ec25f2e3c2a72ffe667c59
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2024-03-20 08:50:09 +0700

    add api for update contact  and unit test

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -135,4 +135,48 @@ class ContactTest extends TestCase
 
     }
 
+    public function testUpdateSuccess(){
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $contact = Contact::query()->limit(1)->first();
+        $this->put('/api/contacts/' . $contact->id, [
+            'first_name' => 'test2',
+            'last_name' => 'test2',
+            'email' => 'test2@gmail.com',
+            'phone' => '12312412322',
+        ],[
+            'Authorization' => 'test'
+        ])->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'first_name' => 'test2',
+                    'last_name' => 'test2',
+                    'email' => 'test2@gmail.com',
+                    'phone' => '12312412322',
+                ]
+            ]);
+
+    }
+
+    public function testUpdateValidationError(){
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $contact = Contact::query()->limit(1)->first();
+        $this->put('/api/contacts/' . $contact->id, [
+            'first_name' => '',
+            'last_name' => 'test2',
+            'email' => 'test2@gmail.com',
+            'phone' => '12312412322',
+        ],[
+            'Authorization' => 'test'
+        ])->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'first_name' => [
+                        'The first name field is required.'
+                    ]
+                ]
+            ]);
+    }
+
+    
+
 }
